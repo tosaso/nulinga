f8dfa29bb2d8cbe366b2a77b67fd02f7d4ced4b8
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-06-26 15:40:04 -0300

    funcionalidad tab

--- my-app/src/components/forms/hour/HourPicker.jsx
+++ my-app/src/components/forms/hour/HourPicker.jsx
@@ -1,33 +1,52 @@
-import React from 'react'
+import React, { useState } from "react"
 import './hourpicker.css'
 
 
 export default function HourPicker() {
+    const types = ['Mañana', 'Tarde', 'Noche'];
     let early =["7hs", "8hs", "9hs", "10hs", "11hs"];
     let late = ["12hs", "13hs", "14hs", "15hs", "16hs"];
     let night = ["17hs", "18hs", "19hs", "20hs", "21hs"]
 
+    const [active, setActive] = useState(types[0])
+
+    function shiftSelected(){
+        if (active === "Mañana") {
+            return early;
+          } else if (active === "Tarde") {
+            return late;
+          }  else {
+            return night;
+          }
+    }
+
     return(
         <>
-        <div className="tab-selector mt2">
-            <div className="tab">Mañana</div>
-            <div className=" selected">Tarde</div>
-            <div className="tab">Noche</div>
-        </div>
-        <div>
-            <form action="" className="hour-picker">
-                {late.map((hour)=>{
-                    return(
-                        <>
-                        <div className="hour">
-                            <input type="radio" id={hour} name="hour-picker" value={hour}></input>
-                            <label for={hour}>{hour}</label>
-                        </div>
-                        </>
-                    )
-                })}
-            </form>
-        </div>
+            <div className="tab-selector mt2">
+                {types.map(type => (
+                <div 
+                    className={active === type ? "selected" : "tab"}
+                    key={type}
+                    active={active === type}
+                    onClick={() => setActive(type)}
+                >
+                    {type}
+                </div>
+                ))}
+            </div>
+            <div>
+                <div className="hour-picker">
+                    {shiftSelected().map((hour)=>{
+                        return(
+                            <>
+                            <form className="hour">
+                                <input type="checkbox" id={hour} name={hour} value={hour}/>{hour}
+                            </form>
+                            </>
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
